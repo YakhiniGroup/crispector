58613e5051b7829b44e5d29e902368e14a265813
iamit <idoamit98@gmail.com>
2019-12-13 18:05:41 +0200

    - Removed unnecessary package files

--- Makefile
+++ Makefile
@@ -45,39 +45,10 @@ clean-pyc: ## remove Python file artifacts
 	find . -name '__pycache__' -exec rm -fr {} +
 
 clean-test: ## remove test and coverage artifacts
-	rm -fr .tox/
-	rm -f .coverage
-	rm -fr htmlcov/
 	rm -fr .pytest_cache
-
-lint: ## check style with flake8
-	flake8 crispector tests
 
 test: ## run tests quickly with the default Python
 	py.test
-
-test-all: ## run tests on every Python version with tox
-	tox
-
-coverage: ## check code coverage quickly with the default Python
-	coverage run --source crispector -m pytest
-	coverage report -m
-	coverage html
-	$(BROWSER) htmlcov/index.html
-
-docs: ## generate Sphinx HTML documentation, including API docs
-	rm -f docs/crispector.rst
-	rm -f docs/modules.rst
-	sphinx-apidoc -o docs/ crispector
-	$(MAKE) -C docs clean
-	$(MAKE) -C docs html
-	$(BROWSER) docs/_build/html/index.html
-
-servedocs: docs ## compile the docs watching for changes
-	watchmedo shell-command -p '*.rst' -c '$(MAKE) -C docs html' -R -D .
-
-release: dist ## package and upload a release
-	twine upload dist/*
 
 dist: clean ## builds source and wheel package
 	python setup.py sdist
--- setup.py
+++ setup.py
@@ -5,39 +5,32 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 # TODO - Should be install via conda or here?? biopython?
 # TODO - pyyaml it's the conda install pyyaml and not "yaml"!!!
 # TODO - edlib & send2trash is fucked up...no version
 requirements = ['Click>=6.0', 'matplotlib>=3.1.0', 'seaborn>=0.9.0','pyyaml>=5.1.2',
                 'numpy>=1.12.1', 'pandas>=0.24.2', 'edlib', 'biopython==1.74', 'scipy>=1.2.1', 'send2trash']
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner']
 
-test_requirements = ['pytest', ]
+test_requirements = ['pytest']
 
 setup(
     author="Ido Amit",
     author_email='idoamit98@gmail.com',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    description="CRISCECTOR - Genome Editing Analysis Tool",
+    description="CRISPECTOR - Genome Editing Analysis Tool",
     entry_points={
         'console_scripts': [
             'crispector=crispector.cli:main',
@@ -45,7 +38,6 @@ setup(
     },
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='crispector',
     name='crispector',
@@ -54,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/iamit87/crispector',
-    version='0.1.0',
+    version='01.0',
     zip_safe=False,
 )
