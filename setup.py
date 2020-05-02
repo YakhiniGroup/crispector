2a8684252d83aa4970978b8195bacab11785ff16
iamit <idoamit98@gmail.com>
2020-05-02 19:11:55 +0300

    - Fixed bug in matplotlib

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     packages=['crispector', 'crispector.algorithm', 'crispector.config', 'crispector.input_processing',
               'crispector.modifications', 'crispector.report', 'crispector.utils'],
     url='https://github.com/YakhiniGroup/crispector',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
